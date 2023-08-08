a1f7a62f09d8df87126cdfe63058df26759b0f2c
Sumathi Kotthuri <kotthuri.sumathi@gmail.com>
2023-08-08 14:02:48 +0000

    added test function

---
+++ main.py
@@ -0,0 +1,4 @@
+"""Module providingFunction printing python version."""
+from projectlib.sample import sample_print
+
+print(sample_print())
---
+++ projectlib/sample.py
@@ -0,0 +1,11 @@
+"""
+I am here a sample module
+"""
+
+
+def sample_print():
+    """
+    I am here to Print
+    """
+    print("Printing...")
+    return "Done"
